9e4f737b32d49e183d908caaedf0758f84755682
jlchn <jianglichn@163.com>
2019-09-19 17:02:33 +0800

    add loader.S

---
+++ boot/create-mbr.sh
@@ -0,0 +1,7 @@
+cd boot
+nasm -I include/ -o mbr.bin mbr.S 
+nasm -I include/ -o loader.bin loader.S 
+dd if=mbr.bin of=hd60.img bs=512 count=1 conv=notrunc
+dd if=loader.bin of=hd60.img bs=512 count=1 seek=2 conv=notrunc
+cd ../
+
--- create-mbr.sh
+++
@@ -1,5 +0,0 @@
-nasm -o mbr.bin mbr.S 
-
-dd if=mbr.bin of=hd60.img bs=512 count=1 conv=notrunc
-
-
