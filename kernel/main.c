40c59ba6c6f2192c1646da15ccab9192b1c26603
jlchn <jianglichn@163.com>
2019-09-20 20:56:30 +0800

    interupt

--- kernel/main.c
+++ kernel/main.c
@@ -3,27 +3,12 @@
 int main()
 {
 
-    int i = 10;
-    while (i-- > 0)
-    {
-        put_char('j');
-        put_char('i');
-        put_char('a');
-        put_char('n');
-        put_char('g');
-        put_char('\n');
-        put_char('l');
-        put_char('i');
-        put_char('\n');
-        put_str("this is kernel codes\n");
-        put_int(90);
-        put_char('\n');
-        put_int(0x00021a3f);
-        put_char('\n');
-        put_int(0x12345678);
-        put_char('\n');
-        put_int(0x00000000);
-    }
+    put_char('j');
+    put_int(20);
+    put_str("\nthis is kernel codes\n");
+
+    init_all();
+    asm volatile("sti"); // 为演示中断处理,在此临时开中断
     while (1)
     {
         /* code */
