95ce90a7682d3a09d9f4bb5762a81f78ed42be3b
jlchn <jianglichn@163.com>
2019-09-20 17:38:10 +0800

    add print functions

--- boot/create-mbr.sh
+++ boot/create-mbr.sh
@@ -4,13 +4,17 @@ nasm -I include/ -o loader.bin loader.S
 dd if=mbr.bin of=hd60.img bs=512 count=1 conv=notrunc
 dd if=loader.bin of=hd60.img bs=512 count=4 seek=2 conv=notrunc
 
-cd ../kernel
-REMOTE_IP=192.168.0.108
-REMOTE_DIR=/home/jiangli/workspace/my-os/
-rsync -az ./main.c $REMOTE_IP:$REMOTE_DIR
-ssh jiangli@$REMOTE_IP 'cd '$REMOTE_DIR';rm-rf *.bin; rm -rf *.o; gcc -c -o main.o main.c; ld main.o -Ttext 0xc0001500 -e main -o kernel.bin; ls -l '
-rsync -az $REMOTE_IP:$REMOTE_DIR'kernel.bin' ./ 
-#gcc -o kernel.bin main.c 
-dd if=kernel.bin of=../boot/hd60.img bs=512 count=200 seek=9 conv=notrunc
+cd ../lib
+#nasm -f elf -o kernel/print.o kernel/print.S
 cd ../
+
+#cd ../kernel
+REMOTE_IP=192.168.0.108
+REMOTE_DIR=/home/jiangli/workspace/
+rsync -az "$PWD" $REMOTE_IP:$REMOTE_DIR
+ssh jiangli@$REMOTE_IP 'cd '$REMOTE_DIR'my-os/;nasm -f elf -o lib/kernel/print.o lib/kernel/print.S; gcc -m32 -I lib/kernel/ -c -o kernel/main.o kernel/main.c; ld -melf_i386  -Ttext 0xc0001500 -e main -o kernel.bin kernel/main.o lib/kernel/print.o; '
+rsync -az $REMOTE_IP:$REMOTE_DIR'my-os/kernel.bin' ./kernel/
+#gcc -o kernel.bin main.c 
+dd if=kernel/kernel.bin of=boot/hd60.img bs=512 count=200 seek=9 conv=notrunc
+
 
--- kernel/main.c
+++ kernel/main.c
@@ -1,6 +1,33 @@
 
+#include "print.h"
 int main()
 {
-    int a = 0;
+
+    int i = 10;
+    while (i-- > 0)
+    {
+        put_char('j');
+        put_char('i');
+        put_char('a');
+        put_char('n');
+        put_char('g');
+        put_char('\n');
+        put_char('l');
+        put_char('i');
+        put_char('\n');
+        put_str("this is kernel codes\n");
+        put_int(90);
+        put_char('\n');
+        put_int(0x00021a3f);
+        put_char('\n');
+        put_int(0x12345678);
+        put_char('\n');
+        put_int(0x00000000);
+    }
+    while (1)
+    {
+        /* code */
+    }
+
     return 0;
 }
