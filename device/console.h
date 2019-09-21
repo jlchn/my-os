afda32b25a3606c991af4c3bbc2c274fc90923f1
jlchn <jianglichn@163.com>
2019-09-21 18:47:18 +0800

    add syscall

--- device/console.h
+++ device/console.h
@@ -4,7 +4,8 @@
 void console_init(void);
 void console_acquire(void);
 void console_release(void);
-void console_put_str(char *str);
+void console_put_str(char* str);
 void console_put_char(uint8_t char_asci);
 void console_put_int(uint32_t num);
 #endif
+
--- kernel/init.c
+++ kernel/init.c
@@ -1,4 +1,3 @@
-
 #include "init.h"
 #include "print.h"
 #include "interrupt.h"
@@ -8,16 +7,17 @@
 #include "console.h"
 #include "keyboard.h"
 #include "tss.h"
+#include "syscall-init.h"
 
 /*负责初始化所有模块 */
-void init_all()
-{
-    put_str("init_all\n");
-    idt_init();
-    timer_init();
-    mem_init();
-    thread_init();
-    console_init();
-    keyboard_init();
-    tss_init();
+void init_all() {
+   put_str("init_all\n");
+   idt_init();	     // 初始化中断
+   mem_init();	     // 初始化内存管理系统
+   thread_init();    // 初始化线程相关结构
+   timer_init();     // 初始化PIT
+   console_init();   // 控制台初始化最好放在开中断之前
+   keyboard_init();  // 键盘初始化
+   tss_init();       // tss初始化
+   syscall_init();   // 初始化系统调用
 }
