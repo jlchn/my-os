7dd0593927ea1f9a5d93e23b690482ce32e592b3
jlchn <jianglichn@163.com>
2019-09-21 17:27:17 +0800

    create and schedule user process

--- kernel/global.h
+++ kernel/global.h
@@ -52,6 +52,15 @@
 #define TSS_ATTR_LOW ((DESC_P << 7) + (DESC_DPL_0 << 5) + (DESC_S_SYS << 4) + DESC_TYPE_TSS)
 #define SELECTOR_TSS ((4 << 3) + (TI_GDT << 2) + RPL0)
 
+//--------------   IDT描述符属性  ------------
+#define IDT_DESC_P 1
+#define IDT_DESC_DPL0 0
+#define IDT_DESC_DPL3 3
+#define IDT_DESC_32_TYPE 0xE // 32位的门
+#define IDT_DESC_16_TYPE 0x6 // 16位的门，不用，定义它只为和32位门区分
+#define IDT_DESC_ATTR_DPL0 ((IDT_DESC_P << 7) + (IDT_DESC_DPL0 << 5) + IDT_DESC_32_TYPE)
+#define IDT_DESC_ATTR_DPL3 ((IDT_DESC_P << 7) + (IDT_DESC_DPL3 << 5) + IDT_DESC_32_TYPE)
+
 struct gdt_desc
 {
     uint16_t limit_low_word;
@@ -62,16 +71,47 @@ struct gdt_desc
     uint8_t base_high_byte;
 };
 
-//--------------   IDT描述符属性  ------------
-#define IDT_DESC_P 1
-#define IDT_DESC_DPL0 0
-#define IDT_DESC_DPL3 3
-#define IDT_DESC_32_TYPE 0xE // 32位的门
-#define IDT_DESC_16_TYPE 0x6 // 16位的门，不用，定义它只为和32位门区分
-#define IDT_DESC_ATTR_DPL0 ((IDT_DESC_P << 7) + (IDT_DESC_DPL0 << 5) + IDT_DESC_32_TYPE)
-#define IDT_DESC_ATTR_DPL3 ((IDT_DESC_P << 7) + (IDT_DESC_DPL3 << 5) + IDT_DESC_32_TYPE)
+//---------------    eflags属性    ----------------
+
+/********************************************************
+--------------------------------------------------------------
+		  Intel 8086 Eflags Register
+--------------------------------------------------------------
+*
+*     15|14|13|12|11|10|F|E|D C|B|A|9|8|7|6|5|4|3|2|1|0|
+*      |  |  |  |  |  | | |  |  | | | | | | | | | | | '---  CF……Carry Flag
+*      |  |  |  |  |  | | |  |  | | | | | | | | | | '---  1 MBS
+*      |  |  |  |  |  | | |  |  | | | | | | | | | '---  PF……Parity Flag
+*      |  |  |  |  |  | | |  |  | | | | | | | | '---  0
+*      |  |  |  |  |  | | |  |  | | | | | | | '---  AF……Auxiliary Flag
+*      |  |  |  |  |  | | |  |  | | | | | | '---  0
+*      |  |  |  |  |  | | |  |  | | | | | '---  ZF……Zero Flag
+*      |  |  |  |  |  | | |  |  | | | | '---  SF……Sign Flag
+*      |  |  |  |  |  | | |  |  | | | '---  TF……Trap Flag
+*      |  |  |  |  |  | | |  |  | | '---  IF……Interrupt Flag
+*      |  |  |  |  |  | | |  |  | '---  DF……Direction Flag
+*      |  |  |  |  |  | | |  |  '---  OF……Overflow flag
+*      |  |  |  |  |  | | |  '----  IOPL……I/O Privilege Level
+*      |  |  |  |  |  | | '-----  NT……Nested Task Flag
+*      |  |  |  |  |  | '-----  0
+*      |  |  |  |  |  '-----  RF……Resume Flag
+*      |  |  |  |  '------  VM……Virtual Mode Flag
+*      |  |  |  '-----  AC……Alignment Check
+*      |  |  '-----  VIF……Virtual Interrupt Flag  
+*      |  '-----  VIP……Virtual Interrupt Pending
+*      '-----  ID……ID Flag
+*
+*
+**********************************************************/
+
+#define EFLAGS_MBS (1 << 1)     // 此项必须要设置
+#define EFLAGS_IF_1 (1 << 9)    // if为1,开中断
+#define EFLAGS_IF_0 0           // if为0,关中断
+#define EFLAGS_IOPL_3 (3 << 12) // IOPL3,用于测试用户程序在非系统调用下进行IO
+#define EFLAGS_IOPL_0 (0 << 12) // IOPL0
 
 #define NULL ((void *)0)
+#define DIV_ROUND_UP(X, STEP) ((X + STEP - 1) / (STEP))
 #define bool int
 #define true 1
 #define false 0
--- kernel/main.c
+++ kernel/main.c
@@ -1,74 +1,70 @@
-
 #include "print.h"
 #include "init.h"
-#include "debug.h"
-#include "memory.h"
 #include "thread.h"
-#include "console.h"
 #include "interrupt.h"
-#include "ioqueue.h"
-#include "keyboard.h"
+#include "console.h"
+#include "process.h"
 
 void k_thread_a(void *);
 void k_thread_b(void *);
-int main()
+void u_prog_a(void);
+void u_prog_b(void);
+int test_var_a = 0, test_var_b = 0;
+
+int main(void)
 {
-
-    put_char('j');
-    put_int(20);
-    put_str("\nthis is kernel codes\n");
-
+    put_str("I am kernel\n");
     init_all();
-    //ASSERT(1 == 2);
-    //asm volatile("sti"); // 为演示中断处理,在此临时开中断
-    void *addr = get_kernel_pages(3);
-    put_str("\n get_kernel_page start vaddr is ");
-    put_int((uint32_t)addr);
-    put_str("\n");
-    void *addr2 = get_kernel_pages(2);
-    put_str("\n get_kernel_page start vaddr2 is ");
-    put_int((uint32_t)addr2);
-    put_str("\n");
 
-    thread_start("consumer_a", 31, k_thread_a, "argA-");
-    thread_start("consu,er_b", 8, k_thread_b, "argB-");
+    thread_start("k_thread_a", 31, k_thread_a, "argA ");
+    thread_start("k_thread_b", 31, k_thread_b, "argB ");
+    process_execute(u_prog_a, "user_prog_a");
+    process_execute(u_prog_b, "user_prog_b");
 
-    intr_enable(); // 打开中断,使时钟中断起作用
+    intr_enable();
     while (1)
-    {
-        // console_put_str("Main ");
-    };
-
+        ;
     return 0;
 }
 
+/* 在线程中运行的函数 */
 void k_thread_a(void *arg)
 {
+    char *para = arg;
     while (1)
     {
-        enum intr_status old_status = intr_disable();
-        if (!ioq_empty(&kbd_buf))
-        {
-            console_put_str(arg);
-            char byte = ioq_getchar(&kbd_buf);
-            console_put_char(byte);
-        }
-        intr_set_status(old_status);
+        console_put_str(" v_a:0x");
+        console_put_int(test_var_a);
+        console_put_str("\n");
     }
 }
 
 /* 在线程中运行的函数 */
 void k_thread_b(void *arg)
 {
+    char *para = arg;
     while (1)
     {
-        enum intr_status old_status = intr_disable();
-        if (!ioq_empty(&kbd_buf))
-        {
-            console_put_str(arg);
-            char byte = ioq_getchar(&kbd_buf);
-            console_put_char(byte);
-        }
-        intr_set_status(old_status);
+        console_put_str(" v_b:0x");
+        console_put_int(test_var_b);
+        console_put_str("\n");
+    }
+}
+
+/* 测试用户进程 */
+void u_prog_a(void)
+{
+    while (1)
+    {
+        test_var_a++;
+    }
+}
+
+/* 测试用户进程 */
+void u_prog_b(void)
+{
+    while (1)
+    {
+        test_var_b++;
     }
 }
